8edd43a8d26675406e03c4ca25a80fb7d03ca398
Дмитрий Францев <Dmitriyfrancev@mail.ru>
2025-05-28 23:11:39 +0300

    pytest-cov install, added tests in test_masks

---
+++ tests/test_masks.py
@@ -0,0 +1,25 @@
+from src.masks import get_mask_card_number, get_mask_account
+import pytest
+
+
+@pytest.mark.parametrize('value, result', [
+    ("7000792289606361", "7000 79** **** 6361"),
+    ("abcd07922xzyr361", "abcd 07** **** r361"),
+    ("", "**** **  ")
+])
+
+def test_get_mask_card_number (value, result):
+    assert get_mask_card_number(value) == result
+
+
+@pytest.fixture
+def account_number():
+    return '73654108430135874305'
+
+
+def test_get_mask_account(account_number):
+    assert get_mask_account(account_number) == '**4305'
+
+
+def test_get_mask_account_empty_number():
+    assert get_mask_account('') == '**'
