cab0932969b8024853cc6f7c82144cc49b072249
Дмитрий Францев <Dmitriyfrancev@mail.ru>
2025-05-31 10:51:39 +0300

    final check

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,5 +1,6 @@
-from src.masks import get_mask_card_number, get_mask_account
 import pytest
+
+from src.masks import get_mask_account, get_mask_card_number
 
 
 @pytest.mark.parametrize('value, result', [
@@ -7,18 +8,18 @@ import pytest
     ("1234abcd07922xzyr361", "1234 ab** **** 2xzy"),
     ("", "**** **  ")
 ])
-def test_get_mask_card_number(value, result):
+def test_get_mask_card_number(value: str, result: str) -> None:
     assert get_mask_card_number(value) == result
 
 
 @pytest.fixture
-def account_number():
+def account_number() -> str:
     return '73654108430135874305'
 
 
-def test_get_mask_account(account_number):
+def test_get_mask_account(account_number: str) -> None:
     assert get_mask_account(account_number) == '**4305'
 
 
-def test_get_mask_account_empty_number():
+def test_get_mask_account_empty_number() -> None:
     assert get_mask_account('') == '**'
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.widget import mask_account_card, get_date
+from src.widget import get_date, mask_account_card
 
 
 @pytest.mark.parametrize('value, result', [
@@ -9,7 +9,7 @@ from src.widget import mask_account_card, get_date
     ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
     ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353")
 ])
-def test_mask_account_card(value, result):
+def test_mask_account_card(value: str, result: str) -> None:
     assert mask_account_card(value) == result
 
 
@@ -18,5 +18,5 @@ def test_mask_account_card(value, result):
     ("2024", "Некорректный формат даты"),
     ("2025-03-11T02:26:18.671407.23149876", "11.03.2025"),
 ])
-def test_get_date(value, result):
+def test_get_date(value: str, result: str) -> None:
     assert get_date(value) == result
