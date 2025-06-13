96d919dbdaa57f2f2381daef4465dc2d20a1cf2a
Дмитрий Францев <Dmitriyfrancev@mail.ru>
2025-06-13 23:12:20 +0300

    Linters check

--- src/decorators.py
+++ src/decorators.py
@@ -1,10 +1,17 @@
-def log(filename=None):
-    def decorator_log(function):
-        def wrapper(*args, **kwargs):
+from typing import Any, Callable
+
+
+def log(filename: None|str=None) -> Callable:
+    """
+    Принимает необязательный параметр filename. Если параметр не указан - выводит логи в консоль.
+    Если указан - вывод логов осуществляется в текстовый файл.
+    """
+    def decorator_log(function: Any) -> Callable:
+        def wrapper(*args: Any, **kwargs: Any) -> None:
             find_error = None
-            result = None
+
             try:
-                result = function(*args, **kwargs)
+                function(*args, **kwargs)
             except Exception as e:
                 find_error = f'{function.__name__} error: {type(e).__name__}. Inputs: {args},{kwargs})\n'
                 # print(find_error)
@@ -14,7 +21,7 @@ def log(filename=None):
                     with open(filename, 'a') as log_file:
                         log_file.write(find_error)
                 else:
-                    with open (filename, 'a') as log_file:
+                    with open(filename, 'a') as log_file:
                         log_file.write(f'{function.__name__} ok\n')
 
             else:
@@ -23,8 +30,6 @@ def log(filename=None):
                 else:
                     print(f'{function.__name__} ok\n')
 
-            return result
+            return None
         return wrapper
     return decorator_log
-
-
